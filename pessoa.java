019ec29ea8afa888dbfa89b5a278e3e0a3c36644
Eliú Vishent <85115233+eliu-vishent@users.noreply.github.com>
2021-06-10 20:20:39 -0300

    Create pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,47 @@
+package atv4;
+
+public class Pessoa {
+	
+	private String nome;
+	private String cpf;
+	private String sexo;
+	
+	public Pessoa(String nome, String cpf, String sexo) {
+		super();
+		this.nome = nome;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	
+	
+	
+	
+	
+
+}
